792fc20ce83a81b141cb23901d856bc7e7ead172
Krystan Ramcharan <krystanramcharan4@gmail.com>
2018-10-22 14:00:59 -0500

    First Question

---
+++ info2180-lab4-master/maze.js
@@ -0,0 +1,25 @@
+  
+  
+  
+  window.onload = mouseOut;
+  
+ 
+ 
+  
+ 
+
+  		
+		document.getElementById("boundary1").onmouseover= function() {mouseOut()};
+		
+
+
+  	   
+		
+		function mouseOut(){
+           document.getElementById("boundary1").classList.add("youlose");
+		   
+  	    }
+  
+	     
+     
+  
